22c01e81db74423d2b46d3cf798029dac5428422
Bart Joy <bart@bart.org.nz>
2013-07-17 23:01:33 +1200

    Initial commit of pages.

---
+++ Script/Site.js
@@ -0,0 +1,36 @@
+﻿var ap = angular.module('AzurePing', []);
+
+ap.controller('AzurePingCtrl', [
+    '$scope',
+    '$http',
+    function ($scope, $http) {
+        $scope.datacentres = [
+            { location: 'West USA (California)', url: 'westusa' },
+            { location: 'North Central USA (Chicago, IL)', url: 'northcentralusa' },
+            { location: 'East USA (Virginia)', url: 'eastusa' },
+            { location: 'North Europe (Dublin, Ireland)', url: 'northeurope' },
+            { location: 'East Asia (Hong Kong, China)', url: 'eastasia' }
+        ];
+
+        $scope.run = function () {
+            angular.forEach($scope.datacentres, function (datacentre) {
+                datacentre.latencyMessage = 'Loading...';
+                var url = 'http://' + datacentre.url + '-azure.azurewebsites.net/Tiny.ashx?callback=JSON_CALLBACK';
+
+                $http.jsonp(url, { cache: false, timeout: 10000 }).error(function (reason) {
+                    datacentre.failureReason = reason;
+                }).success(function () {
+                    var start = new Date().getTime();
+                    $http.jsonp(url, { cache: false, timeout: 10000 }).error(function (reason) {
+                        datacentre.failureReason = reason;
+                    }).success(function () {
+                        var complete = new Date().getTime();
+                        var elapsed = complete - start;
+                        datacentre.latencyMilliseconds = elapsed;
+                        datacentre.latencyMessage = elapsed + " ms";
+                    });
+                });
+            });
+        };
+    }
+]);
---
+++ Script/Site.ts
@@ -0,0 +1,52 @@
+﻿/// <reference path="../definitelytyped/angularjs/angular.d.ts" />
+
+var ap = angular.module('AzurePing', []);
+
+interface IAzureDatacentre {
+    location: string;
+    url: string;
+    latencyMessage?: string;
+    latencyMilliseconds?: number;
+    failureReason?: string;
+}
+
+interface IAzurePingScope extends ng.IScope {
+    datacentres: IAzureDatacentre[];
+    run(): void;
+}
+
+ap.controller('AzurePingCtrl', ['$scope', '$http',
+    ($scope: IAzurePingScope, $http: ng.IHttpService) => {
+        $scope.datacentres = [
+            { location: 'West USA (California)', url: 'westusa' },
+            { location: 'North Central USA (Chicago, IL)', url: 'northcentralusa' },
+            { location: 'East USA (Virginia)', url: 'eastusa' },
+            { location: 'North Europe (Dublin, Ireland)', url: 'northeurope' },
+            { location: 'East Asia (Hong Kong, China)', url: 'eastasia' }
+        ];
+
+        $scope.run = () => {
+            angular.forEach($scope.datacentres, (datacentre: IAzureDatacentre) => {
+                datacentre.latencyMessage = 'Loading...'
+                var url = 'http://' + datacentre.url + '-azure.azurewebsites.net/Tiny.ashx?callback=JSON_CALLBACK';
+                //Hit the page twice, but only grab the second reading. This ensures DNS is resolved etc.
+                $http.jsonp(url, { cache: false, timeout: 10000 })
+                    .error((reason) => {
+                        datacentre.failureReason = reason;
+                    })
+                    .success(() => {
+                        var start = new Date().getTime();
+                        $http.jsonp(url, { cache: false, timeout: 10000 })
+                            .error((reason) => {
+                                datacentre.failureReason = reason;
+                            })
+                            .success(() => {
+                                var complete = new Date().getTime()
+                                var elapsed = complete - start;
+                                datacentre.latencyMilliseconds = elapsed;
+                                datacentre.latencyMessage = elapsed + " ms";
+                            });
+                    });
+            });
+        };
+    }]);
